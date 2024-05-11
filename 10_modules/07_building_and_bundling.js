c7f75ea335a936228b3bb4b398d6f520a44a461d
hamidouwaraballo <ballowara@gmail.com>
2024-05-11 01:59:06 -0400

    modules

---
+++ 10_modules/07_building_and_bundling.js
@@ -0,0 +1,4 @@
+// Bundlers patch all modules together into a single Big File. File can be big so javascript 
+// programmers created "minifiers", they remove white spaces comments and replace pieces of code
+// with equivalents ones that takes up less space. Tools like this exists take a look at them when
+// needed
